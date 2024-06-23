abd1e9d8293be95631d55b3a2afc4d0b882f47a0
Edison Zhang <edisonzhang@ucsb.edu>
2024-06-22 19:22:21 -0700

    added link to drive page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,16 +10,22 @@ export default function Home() {
 
       <div className="content">
         <h1 className="main-text">
-          Experience safe night driving with{' '}
-          <span style={{
-          fontSize: '40px',
-          color: "#9B9DFF",
-          WebkitTextStroke: '1px #393cb8', 
-        }}>driveX</span>
+          Experience safe night driving with{" "}
+          <span
+            style={{
+              fontSize: "40px",
+              color: "#9B9DFF",
+              WebkitTextStroke: "1px #393cb8",
+            }}
+          >
+            driveX
+          </span>
           <div className="button-container">
-          <button className="gradient-button">Let's drive</button>
-         <p className="offer-text"> 	ℹ️ see what we offer</p> 
-         </div>
+            <button className="gradient-button">
+              <Link href="/drive">Let's drive</Link>
+            </button>
+            <p className="offer-text"> ℹ️ see what we offer</p>
+          </div>
         </h1>
       </div>
     </div>
